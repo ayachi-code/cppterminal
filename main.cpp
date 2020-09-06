90341414cb047b133db6ecfd8097e529f1998821
Bilal ayachi <bilal.ayachi@hetmml.nl>
2020-09-06 15:10:54 +0200

    [add[ checkUserinput returns users input

--- main.cpp
+++ main.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
+class Commands {
+    public:
+        string checkUserInput(string userCommand) {
+            return userCommand;
+        }
+ };
+
 int main() {
+    Commands userInput;
     string command = "pwd";
     string quit_key = "q";
     cout << "PLACEHOLDER-PC:";
     cin >> command;
     while (command != quit_key) {
         cout << "PLACEHOLDER-PC:";
+        //executes functions
+        string foo = userInput.checkUserInput(command);
+        cout << foo;
         cin >> command;
     }
 
